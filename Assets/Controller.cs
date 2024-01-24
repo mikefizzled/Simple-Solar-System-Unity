e3e8b0748c71b5f5aed3803c756feb09eaf46ff0
MichaelReaneyShu <michael.reaney3@student.shu.ac.uk>
2024-01-24 16:17:59 +0000

    universal logo

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Controller : MonoBehaviour
@@ -17,57 +18,64 @@ public class Controller : MonoBehaviour
     public float earthSize = 100.0f;
     public float birdSize = 100f;
     public float multiplier = 5f;
+    public bool boost = false;
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Keypad1)) {
+        if (Input.GetKey(KeyCode.LeftShift))
+            boost = true;
+        else
+            boost = false;
+
+        if(boost)
+            multiplier = 5.0f;
+        else
+            multiplier = 1f;
+
+        if(Input.GetKeyDown(KeyCode.Keypad1)||Input.GetKeyDown(KeyCode.Alpha1)) {
             earth.enabled = false;
             birdsEye.enabled = true;
             current = birdsEye;
         }
-        if(Input.GetKeyDown(KeyCode.Keypad2)) { 
+        if(Input.GetKeyDown(KeyCode.Keypad2)||Input.GetKeyDown(KeyCode.Alpha2)) { 
             birdsEye.enabled = false; 
             earth.enabled = true;
             current = earth;
         }
 
-        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
             current.orthographicSize -= speed * Time.deltaTime * multiplier;
-        else if (Input.GetKey(KeyCode.UpArrow))
-            current.orthographicSize -= speed * Time.deltaTime;
         
-        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             current.orthographicSize += speed * Time.deltaTime * multiplier;
-        }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            current.orthographicSize += speed * Time.deltaTime;
         }
         if (birdsEye.enabled)
         {
             if (Input.GetKey(KeyCode.A))
-                current.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+                current.transform.position -= new Vector3(speed * Time.deltaTime * multiplier, 0, 0);
             if (Input.GetKey(KeyCode.D))
-                current.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+                current.transform.position += new Vector3(speed * Time.deltaTime * multiplier, 0, 0);
             if (Input.GetKey(KeyCode.W))
-                current.transform.position += new Vector3(0, 0, speed * Time.deltaTime);
+                current.transform.position += new Vector3(0, 0, speed * Time.deltaTime * multiplier);
             if (Input.GetKey(KeyCode.S))
-                current.transform.position -= new Vector3(0, 0, speed * Time.deltaTime);
+                current.transform.position -= new Vector3(0, 0, speed * Time.deltaTime * multiplier);
         }
-            if (Input.GetKey(KeyCode.R))
-                {
-            earth.transform.position = earthStart;
-            earth.orthographicSize = earthSize;
-            birdsEye.transform.position = birdStart;
-            birdsEye.orthographicSize = birdSize;
-            earth.enabled = false;
-            birdsEye.enabled = true;
-            current = birdsEye;
-         }
+
+        if (Input.GetKey(KeyCode.R))
+            {
+                earth.transform.position = earthStart;
+                earth.orthographicSize = earthSize;
+                birdsEye.transform.position = birdStart;
+                birdsEye.orthographicSize = birdSize;
+                earth.enabled = false;
+                birdsEye.enabled = true;
+                current = birdsEye;
+        }
     }
 }
